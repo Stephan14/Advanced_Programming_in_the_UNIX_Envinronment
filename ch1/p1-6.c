ee645faa420925e4c4b523be40502ce20c9fe91d
stephan14 <zsf657135507@gmail.com>
2016-11-07 23:27:14 +0800

    信号处理函数

---
+++ ch1/p1-6.c
@@ -0,0 +1,46 @@
+#include "apue.h"
+#include "myerr.h"
+#include <sys/wait.h>
+
+static void sig_int( int );
+
+int 
+main( int argc, char *argv[] )
+{
+	char buf[MAXLINE];
+	pid_t pid;
+	int status;
+	
+	if( signal( SIGINT, sig_int) == SIG_ERR )
+		err_sys("signal error");		
+	
+	printf("%%");
+	while( fgets( buf, MAXLINE, stdin) != NULL )
+	{
+		if( buf[ strlen( buf ) - 1] == '\n' )
+			//用null代替换行符
+			buf[ strlen( buf ) - 1 ] = 0;
+		
+		if( ( pid = fork() ) < 0 )
+		{
+			err_sys("fork error");
+		}//子进程
+		else if( pid == 0 )
+		{
+			execlp( buf, buf, (char*) 0);
+			err_ret("couldn't execute: %s ", buf );
+			exit( 127 );
+		}
+		//等待进程ID为pid的进程终止
+		if( pid = waitpid( pid, &status, 0) < 0 )
+			err_sys("waitup error");	
+		printf("%%");
+	}	
+	return 0;
+}
+
+void 
+sig_int( int signal )
+{
+	printf("interrupt\n%% ");
+}
